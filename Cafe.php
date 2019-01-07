696fbc12e96b74533eac9565e34090aae5e38189
A Yano <ATYcreateaChiya619@gmail.com>
2019-01-07 11:39:47 +0900

    お店画面、画像フォルダ作成

---
+++ Cafe.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>お店</title>
+		
+		<style>
+		#game-window
+		{			
+		}
+		
+		#menu0
+		{
+			width:280px;
+			position:absolute;
+			top:60px;
+			left:60px;
+		}
+		#menu1
+		{
+			width:280px;
+			position:absolute;
+			top:60px;
+			left:360px;
+		}
+		#menu2
+		{
+			width:280px;
+			position:absolute;
+			top:60px;
+			left:660px;
+		}
+		</style>
+		
+	</head>
+
+	<body>
+		<div id="game-window">
+			<img id="menu0" src="assets/name_neko.png">
+			<img id="menu1" src="assets/牡蠣L.png">
+			<img id="menu2" src="assets/秋刀魚R.png">
+		</div>
+		
+		<script>
+			gw=document.querySelector("#game-window");
+		</script>
+		
+	</body>
+</html>
