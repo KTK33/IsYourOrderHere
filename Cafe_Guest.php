553f7a9fa2f44ef639b62b95874b6ad2c609602b
KTK33 <lejyend7890@ezweb.ne.jp>
2019-01-21 13:58:38 +0900

    客メニュー更新

--- Cafe_Guest.php
+++ Cafe_Guest.php
@@ -2,7 +2,7 @@
 
 <html>
 	<head>
-		<title>お店</title>
+		<title>客メニュー</title>
 		
 		<style>
 		#game-window
@@ -68,20 +68,54 @@
     		border: solid 2p green;
 		}
 		
+		#gazo{
+			position: absolute;
+			top: 20%; 
+			left : 70%;		
+		}
 		</style>
 		
+		<script>
+		
+			function Initialize(){
+			var n=0;
+			document.getElementById("Coffee").disabled = false;
+			document.getElementById("Caramel").disabled = false;
+			document.getElementById("IceCoffee").disabled = false;
+			}
+
+		</script>
 	</head>
 
 	<body>
 		<div id="game-window">
 			<img id="menuBook"src="assets/menu.png">
+			<img id="gazo" name="gazo">
 		</div>
-			<input type="button"style="WIDTH: 200px; HEIGHT: 50px" id="Coffee" value="コーヒー">
-			<input type="button"style="WIDTH: 200px; HEIGHT: 50px" id="IceCoffee" value="アイスコーヒー">
-			<input type="button"style="WIDTH: 200px; HEIGHT: 50px" id="Caramel" value="キャラメル">
+			<input type="button"style="WIDTH: 200px; HEIGHT: 50px" id="Coffee" value="コーヒー" onClick="Decision(1);">
+			<input type="button"style="WIDTH: 200px; HEIGHT: 50px" id="Caramel" value="キャラメル" onClick="Decision(2);">
+			<input type="button"style="WIDTH: 200px; HEIGHT: 50px" id="IceCoffee" value="アイスコーヒー" onClick="Decision(3);">
 			
-		<script>
+		<script script language="javascript" type="text/javascript">
 			gw=document.querySelector("#game-window");
+							
+			function Decision(NUM){
+				n = NUM;
+				
+				if ( NUM == 1 ) {
+					document.getElementById("gazo").src="assets/coffee.png";
+					document.getElementById("gazo").alt="コーヒー";
+				}
+				else if( NUM == 2 ){
+					document.getElementById("gazo").src="assets/caramel.png";
+					document.getElementById("gazo").alt="キャラメル";
+				}
+				else if( NUM == 3 ){
+					document.getElementById("gazo").src="assets/Ice_coffee.png";
+					document.getElementById("gazo").alt="アイスコーヒー";
+				}
+			}
+
 		</script>
 		
 	</body>
---
+++ Good.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>お店</title>
+		
+		<style>
+		#game-window
+		{		
+			background-image:url(assets/CafeBack.jpg);
+			background-size:cover;
+			height:800px;
+		}
+		
+		#menu1_img
+		{
+			width:280px;
+			position:absolute;
+			top:160px;
+			left:810px;
+		}
+		#menu1_button
+		{
+			width:200px; height:50px;
+			position:absolute;
+			top:460px;
+			left:825px;
+			font-size:16pt;
+		}
+		
+		</style>
+		
+	</head>
+
+	<body>
+		<div id="game-window">
+			<img id="menu1_img" src="assets/caramel.png">
+		</div>
+		
+	</body>
+</html>
